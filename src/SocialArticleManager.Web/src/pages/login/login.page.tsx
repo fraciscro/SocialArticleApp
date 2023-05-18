16e27b027d30667af9d03af6f0d5558db2e9bad5
fraciscro <you@example.com>
2023-05-18 15:18:00 +0100

    added

--- src/SocialArticleManager.Web/src/pages/login/login.page.tsx
+++ src/SocialArticleManager.Web/src/pages/login/login.page.tsx
@@ -4,73 +4,71 @@ export const LoginPage = () => {
   return (
     <>
       <section className="vh-100">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-6 text-black">
-              <div className="px-5 ms-xl-4">
-                <i
-                  className="fas fa-crow fa-2x me-3 pt-5 mt-xl-4"
-                  style={{ color: "#709085" }}
-                />
-                <span className="h1 fw-bold mb-0">Logo</span>
-              </div>
-              <div className="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-                <form style={{ width: "23rem" }}>
-                  <h3
-                    className="fw-normal mb-3 pb-3"
-                    style={{ letterSpacing: 1 }}
-                  >
-                    Log in
-                  </h3>
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div
+                className="card shadow-2-strong"
+                style={{ borderRadius: "1rem" }}
+              >
+                <div className="card-body p-5 text-center">
+                  <h3 className="mb-5">Sign in</h3>
                   <div className="form-outline mb-4">
                     <input
                       type="email"
-                      id="form2Example18"
+                      id="typeEmailX-2"
                       className="form-control form-control-lg"
                     />
-                    <label className="form-label" htmlFor="form2Example18">
-                      Email address
+                    <label className="form-label" htmlFor="typeEmailX-2">
+                      Email
                     </label>
                   </div>
                   <div className="form-outline mb-4">
                     <input
                       type="password"
-                      id="form2Example28"
+                      id="typePasswordX-2"
                       className="form-control form-control-lg"
                     />
-                    <label className="form-label" htmlFor="form2Example28">
+                    <label className="form-label" htmlFor="typePasswordX-2">
                       Password
                     </label>
                   </div>
-                  <div className="pt-1 mb-4">
-                    <button
-                      className="btn btn-info btn-lg btn-block"
-                      type="button"
-                    >
-                      Login
-                    </button>
+                  {/* Checkbox */}
+                  <div className="form-check d-flex justify-content-start mb-4">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      defaultValue=""
+                      id="form1Example3"
+                    />
+                    <label className="form-check-label" htmlFor="form1Example3">
+                      {" "}
+                      Remember password{" "}
+                    </label>
                   </div>
-                  <p className="small mb-5 pb-lg-2">
-                    <a className="text-muted" href="#!">
-                      Forgot password?
-                    </a>
-                  </p>
-                  <p>
-                    Don't have an account?{" "}
-                    <a href="#!" className="link-info">
-                      Register here
-                    </a>
-                  </p>
-                </form>
+                  <button
+                    className="btn btn-primary btn-lg btn-block"
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                  <hr className="my-4" />
+                  <button
+                    className="btn btn-lg btn-block btn-primary"
+                    style={{ backgroundColor: "black" }}
+                    type="submit"
+                  >
+                    <i className="fab fa-apple" />
+                  </button>
+                  <button
+                    className="btn btn-lg btn-block btn-primary mb-2"
+                    style={{ backgroundColor: "#3b5998" }}
+                    type="submit"
+                  >
+                    <i className="fab fa-facebook" />
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="col-sm-6 px-0 d-none d-sm-block">
-              <img
-                src="../../../public/mestre.png"
-                alt="Login image"
-                className="w-100 vh-100"
-                style={{ objectFit: "cover", objectPosition: "left" }}
-              />
             </div>
           </div>
         </div>
