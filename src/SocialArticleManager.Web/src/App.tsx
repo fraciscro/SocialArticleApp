b09a71b637e9824023aa921815e1cbd328ecba7f
fraciscro <you@example.com>
2023-05-19 23:40:54 +0100

    added login & register page

--- src/SocialArticleManager.Web/src/App.tsx
+++ src/SocialArticleManager.Web/src/App.tsx
@@ -4,6 +4,8 @@ import { Layout } from "./common/layout/layout";
 import { ArticlePage } from "./pages/article/article.page";
 import { OrganizationPage } from "./pages/organization/organization.page";
 import { SearchPage } from "./pages/search/search.page";
+import { LoginPage } from "./pages/auth/login.page";
+import { RegisterPage } from "./pages/auth/register.page";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<ArticlePage />} />
           <Route path="/organizations/:id" element={<OrganizationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/search" element={<SearchPage />} />
         </Route>
       </Routes>
